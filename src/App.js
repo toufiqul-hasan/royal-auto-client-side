20e29efd31316baca179df327892c09621accda3
toufiqul-hasan <toufiqul.shihab@gmail.com>
2022-04-29 09:07:06 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./Components/About/About";
+import Blogs from "./Components/Blogs/Blogs";
+import Header from "./Components/Header/Header";
+import Home from "./Components/Home/Home";
 
 function App() {
   return (
     <div>
-      
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import logo from "../../Assets/Images/logo.png";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <>
+    <Navbar collapseOnSelect expand="lg" sticky="top"  bg="light" variant="light">
+    <Container>
+    <Navbar.Brand as={Link} to="/"><img className="d-inline-block align-top" width={30} height={30} src={logo} alt="logo" /><span className='logo'>Royal Auto</span></Navbar.Brand>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="me-auto" />
+    <Nav className="nav">
+    <Nav.Link as={Link} to="home">Home</Nav.Link>
+    <Nav.Link as={Link} to="blogs">Blogs</Nav.Link>
+    <Nav.Link as={Link} to="about">About</Nav.Link>
+    </Nav>
+    </Navbar.Collapse>
+    </Container>
+  </Navbar>
+  </>
+  );
+};
+
+export default Header;
