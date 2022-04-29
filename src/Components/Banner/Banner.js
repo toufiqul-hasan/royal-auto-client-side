ae69398150f3f86bce8774c3b1982d8d71131d7b
toufiqul-hasan <toufiqul.shihab@gmail.com>
2022-04-29 11:32:03 +0600

    banner added

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Banner.css";
+const Banner = () => {
+  return (
+    <div className="banner-section">
+      <div className="container">
+        <div className="zindex">
+          <div className="row">
+            <div className="col-lg-4">
+              <div className="banner-content">
+                <h1>Find Your Next Car at Royal Auto</h1>
+                <p>
+                  Allow us to guide you through the innovative stress free
+                  approach in finding your dream car.
+                </p>
+                <button className="button">Learn More</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from "react";
+import Banner from "../Banner/Banner";
+import "./Home.css";
 
 const Home = () => {
   return (
     <div>
-      
+      <div>
+        <Banner />
+      </div>
     </div>
   );
 };
